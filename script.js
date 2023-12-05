61785f70339aa8c6fbfe328661aae1baf9373099
mindsetpro_ <138173273+mindsetpro@users.noreply.github.com>
2023-12-05 18:22:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,89 @@
+// Add a list to store file and folder information
+let fileSystem = [
+  { name: 'Folder 1', type: 'folder', children: [] },
+  { name: 'File 1.txt', type: 'file', content: 'This is the content of File 1.txt' },
+];
+
+function openFileManager() {
+  const fileManagerDiv = document.getElementById('fileManager');
+  fileManagerDiv.style.display = 'block';
+
+  // Display the current file system
+  displayFileSystem();
+}
+
+function createFolder() {
+  const folderName = prompt('Enter folder name:');
+  if (folderName) {
+    const newFolder = { name: folderName, type: 'folder', children: [] };
+    fileSystem.push(newFolder);
+    displayFileSystem();
+  }
+}
+
+function uploadFile() {
+  const fileInput = document.getElementById('fileInput');
+  fileInput.click();
+}
+
+function handleFileUpload() {
+  const fileInput = document.getElementById('fileInput');
+  const files = fileInput.files;
+
+  for (const file of files) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const newFile = { name: file.name, type: 'file', content: e.target.result };
+      fileSystem.push(newFile);
+      displayFileSystem();
+    };
+    reader.readAsText(file);
+  }
+
+  // Reset file input to allow uploading the same file again
+  fileInput.value = '';
+}
+
+function deleteItem(index) {
+  fileSystem.splice(index, 1);
+  displayFileSystem();
+}
+
+function displayFileSystem() {
+  const fileList = document.getElementById('fileList');
+  fileList.innerHTML = '';
+
+  for (let i = 0; i < fileSystem.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      ${fileSystem[i].type === 'folder' ? '📁' : '📄'} ${fileSystem[i].name}
+      <button onclick="deleteItem(${i})">Delete</button>
+    `;
+
+    if (fileSystem[i].type === 'folder') {
+      listItem.addEventListener('click', () => openFolder(i));
+    }
+
+    fileList.appendChild(listItem);
+  }
+}
+
+function openFolder(index) {
+  const folder = fileSystem[index];
+  if (folder.type !== 'folder') {
+    alert(`${folder.name} is not a folder.`);
+    return;
+  }
+
+  const folderContent = folder.children;
+  if (folderContent.length === 0) {
+    alert(`${folder.name} is empty.`);
+  } else {
+    let contentText = `Contents of Folder ${folder.name}:\n\n`;
+    for (const item of folderContent) {
+      contentText += `${item.type === 'folder' ? '📁' : '📄'} ${item.name}\n`;
+    }
+    alert(contentText);
+  }
+}
+
